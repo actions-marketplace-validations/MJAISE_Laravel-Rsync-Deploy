26b12a272727c737624fdb288f3f67800d648a46
Mohamed Jasim Ahmed <34663944+MJAISE@users.noreply.github.com>
2020-10-26 07:49:45 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,8 @@ SERVER_DEPLOY_STRING="$REMOTE_USER@$REMOTE_HOST:$TARGET_DIRECTORY"
 
 # Run Rsync synchronization
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_HOST_PORT' . $SERVER_DEPLOY_STRING"
+ssh -i /root/.ssh/id_rsa -t $REMOTE_USER@$REMOTE_HOST "sudo chown -R $REMOTE_USER:$REMOTE_USER $TARGET_DIRECTORY"
+ssh -i /root/.ssh/id_rsa -t $REMOTE_USER@$REMOTE_HOST "sudo chmod 775 -R $TARGET_DIRECTORY"
+ssh -i /root/.ssh/id_rsa -t $REMOTE_USER@$REMOTE_HOST "sudo chmod 777 -R $TARGET_DIRECTORY/storage"
+ssh -i /root/.ssh/id_rsa -t $REMOTE_USER@$REMOTE_HOST "sudo chmod 777 -R $TARGET_DIRECTORY/public"
+ssh -i /root/.ssh/id_rsa -t $REMOTE_USER@$REMOTE_HOST "cd $TARGET_DIRECTORY && php artisan migrate && php artisan modelcach:clear && php artisan cache:clear && php artisan route:cache && php artisan config:cache"
